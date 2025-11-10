0de1012e507bad555af073a79478bd4f2578494a
Phan Thanh Tu <tulklk32@gmail.com>
2025-11-11 03:07:24 +0700

    fixxxx

---
+++ EVChargingStationManagement-FinalProject/DataAccessLayer/Migrations/20251110200448_UpdateDatabaseSchema.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BookingId",
+                table: "tbl_charging_session",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndedAt",
+                table: "tbl_charging_session",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "EnergyKwh",
+                table: "tbl_charging_session",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PricePerKwh",
+                table: "tbl_charging_session",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartedAt",
+                table: "tbl_charging_session",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "tbl_charging_session",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookingId",
+                table: "tbl_charging_session");
+
+            migrationBuilder.DropColumn(
+                name: "EndedAt",
+                table: "tbl_charging_session");
+
+            migrationBuilder.DropColumn(
+                name: "EnergyKwh",
+                table: "tbl_charging_session");
+
+            migrationBuilder.DropColumn(
+                name: "PricePerKwh",
+                table: "tbl_charging_session");
+
+            migrationBuilder.DropColumn(
+                name: "StartedAt",
+                table: "tbl_charging_session");
+
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "tbl_charging_session");
+        }
+    }
+}
